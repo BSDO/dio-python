5a52aeff6d01a3a8e31b3e30d70093760616de1e
brenobarontech <breno.silva@barontech.com.br>
2023-10-30 22:33:50 -0300

    Aulas de listas

---
+++ Dominando Python/listas.py
@@ -0,0 +1,62 @@
+# Listas em py
+
+# listar = ['Breno',['lista','teste'],['lisar']]
+# print(listar[1][1])
+
+# index para achar um dado
+
+# listar = ['tv','cama','radio','computador','geladeira','fogao','armario','tapate']
+
+# item = str(input("Digite o item que procura"))
+
+# if item in listar:
+#     i = listar.index(item)
+#     print(f'Item: {i}')
+# else:
+#     print("Produto nao existe:")
+
+
+#adicionar ou remove items
+
+
+# listar = ['tv','cama','radio','computador','geladeira','fogao','armario','tapate']
+
+# nmr =  input("Digite um numero: ")
+# listar.append(nmr)
+# print("-----------------\n")
+# print(listar)
+
+# listar.remove('fogao')
+# print(listar)
+# print("-----------------\n")
+# print("-----------------\n")
+# listar.pop(2)
+# print(listar)
+
+
+# #funcao try e except pra verifica 
+
+# try:
+#     listar.remove('fogaooooo')
+# except:
+#     print("Não existe")
+   
+
+# funcao de maior,menor e len
+# listar = [20,600,588,699,7444,2563,0,36]
+# print(f"Tamanho dessa listar : {len(listar)}")
+# print("-----------------\n")
+# item_maior = max(listar)
+# print(f"O maior item: {item_maior}")
+# print("-----------------\n")
+# item_menor = min(listar)
+# print(f"O maior item: {item_menor}")
+# print("-----------------\n")
+# print(f"A posicao do meu maior item: {listar.index(item_maior)}")
+# print("-----------------\n")
+# print(f"A posicao do meu menor item: {listar.index(item_menor)}")
+
+#juntar listar, ordernar e cuidados especiais
+
+      
+
